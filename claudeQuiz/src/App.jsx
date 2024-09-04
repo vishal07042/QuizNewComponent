9d2e30dc471024c426a9231457a1db44c78ad0da
vishal mehta <professionalprovishal@gmail.com>
2024-09-04 10:10:47 +0530

    Components to add in quiz app

---
+++ claudeQuiz/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+import React, { useState } from 'react';
+import DSADashboard from './components/DashBoard';
+import QuizSection from './components/DashBoard';
+import DragDrop2 from './components/DragDrop2';
+
+const App = () => {
+  return (
+    <div>
+      <DragDrop2 />
+    </div>
+  );
+};
+
+export default App;
